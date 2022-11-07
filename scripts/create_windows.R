83d83e3c805af8d10c05a998b860c22db9731209
SilviaBuonaiuto <buonaiutosilvia@gmail.com>
2022-11-07 16:01:40 +0100

    modified create_windows.R

--- scripts/create_windows.R
+++ scripts/create_windows.R
@@ -14,4 +14,4 @@ chromosome = args[3]
 #evaluate regions length, select the ones with length > than window size and put them in vectors
 regions<-mask %>% mutate(length = end-start) %>% filter(length >= as.numeric(dimension)) %>% rowwise() %>% mutate(my_seq = map2(start, end, seq, by=as.numeric(dimension))) %>% pull(my_seq)
 #create dataframe with windows and write in a tab separated file (bed file)
-data.frame(unlist(regions)) %>% rename(start = "unlist.regions.") %>% mutate(CHROM = paste(chromosome), end = start+1000) %>% select(CHROM, start, end) %>% write.table(args[4], sep = "\t", quote = F, col.names = F, row.names = F)
+data.frame(unlist(regions)) %>% rename(start = "unlist.regions.") %>% mutate(CHROM = paste(chromosome), end = start+as.numeric(dimension)) %>% select(CHROM, start, end) %>% write.table(args[4], sep = "\t", quote = F, col.names = F, row.names = F)
