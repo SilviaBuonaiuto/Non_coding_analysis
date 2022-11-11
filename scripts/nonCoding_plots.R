84422d3ddaa9f25eb40f54485e4467bea125fa3c
SilviaBuonaiuto <buonaiutosilvia@gmail.com>
2022-11-11 11:37:02 +0100

    updated nonCoding_plots.R

--- scripts/nonCoding_plots.R
+++ scripts/nonCoding_plots.R
@@ -7,7 +7,7 @@ library(DescTools)
 
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
- stop('specify five arguments: <common> <rare> <nucleotide_counts> <window_size> <color> <prefix for output plot and table> ', call.=FALSE)
+ stop('specify five arguments: <common> <rare> <nucleotide_counts> <path to output file> <window_size> <color> <prefix for output plot and table> ', call.=FALSE)
 } 
 
 #open input files, add column names and merge common and rare count tables
@@ -23,7 +23,7 @@ all<- merge(rare, common, by = c("chrom", "start", "end"))
 
 # merge rare/common table and table with nucleotides numbers
 final<-all %>% unite(chrpos, c("chrom", "start"), sep = ":", remove = F) %>% unite(Gene, c("chrpos", "end"), sep = "-", remove = F) %>% merge(nuc, by = "Gene") %>% select(-chrpos)
-print(head(final))
+final %>% write.table(args[4], sep = "\t", quote = F, col.names = T, row.names = F)
 ### calculate rare and common frequencies
 rareProb<-final %>% mutate(med = (rareCounts/(rareCounts+commonCounts))) %>% filter(rareCounts+commonCounts >0) %>% summarise(median(med))
 print(rareProb)
@@ -31,12 +31,12 @@ commonProb<-final %>% mutate(med = (commonCounts/(rareCounts+commonCounts))) %>%
 print(commonProb)
 
 # calculate probabilities of finding windows with excess of rare variants and plot all probabilities
-window_size = args[4]
-mycol = args[5]
+window_size = args[5]
+mycol = args[6]
 all %>% mutate(pbinom = pbinom(rareCounts, rareCounts + commonCounts, as.numeric(rareProb))) %>% filter(pbinom != 1) %>% ggplot(aes(start, -log10(1-pbinom))) +geom_point(aes(alpha = 0.3 ), color = mycol ) + theme_bw() + scale_alpha(guide = 'none') + ylim(0,6) + ggtitle(paste(code, window_size, sep = "-"))
-ggsave(paste(args[6], "pbinom.png", sep = "."), width = 14, heigh = 4) 
+ggsave(paste(args[7], "pbinom.png", sep = "."), width = 14, heigh = 4) 
 #create table containing windows with excess of rare variants
-all %>% mutate(pbinom = pbinom(rareCounts, rareCounts + commonCounts, as.numeric(rareProb))) %>% mutate(log1pbinom = -log10(1-pbinom)) %>% filter(pbinom != 1) %>% filter(log1pbinom > 4) %>% write.table(paste(args[6], "tsv", sep = "."), sep = "\t",quote = F, col.names=T, row.names=F)
+all %>% mutate(pbinom = pbinom(rareCounts, rareCounts + commonCounts, as.numeric(rareProb))) %>% mutate(log1pbinom = -log10(1-pbinom)) %>% filter(pbinom != 1) %>% filter(log1pbinom > 4) %>% write.table(paste(args[7], "tsv", sep = "."), sep = "\t",quote = F, col.names=T, row.names=F)
 
 #Linear regression of observed data
 lmObs<-lm(rareCounts~ A+C+G+CG , data = final)
@@ -54,7 +54,7 @@ modExp = lmExp$coefficients[1]+lmExp$coefficients[2]*numA+lmExp$coefficients[3]*
 
 #Gtest to compare observed and expected and plot
 final %>% mutate(normO = (modObs-min(modObs))/(max(modObs)-min(modObs)), normE = (modExp-min(modExp))/(max(modExp)-min(modExp))) %>% rowwise() %>% mutate(gtest = GTest(cbind(normO, normE))$p.value) %>% ggplot(aes(start, -log10(gtest))) + geom_point() + theme_bw() + ylab("-log10(p-value)")
-ggsave(paste(args[6], "gtest.png", sep = "."), width = 10, heigh = 4)
+ggsave(paste(args[7], "gtest.png", sep = "."), width = 10, heigh = 4)
 
 
 
